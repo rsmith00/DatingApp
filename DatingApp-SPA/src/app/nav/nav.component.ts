0288757d090c416f3dc3be51e4c370ce756c6d69
Rob Smith <rsmith00@outlook.com>
2020-07-13 14:11:23 -0500

    Adding router guards.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,7 +13,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+              private router: Router) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
@@ -24,6 +26,8 @@ login(){
       this.alertify.success('Logged in successfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -36,5 +40,6 @@ login(){
   logout(){
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
