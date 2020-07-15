67384541742be4cbae1b714dde6c528f15f18ac7
Rob Smith <rsmith00@outlook.com>
2020-07-15 10:20:29 -0500

    Extending api to pull list of users in more detail

--- DatingApp.api/Dtos/UserForDetailedDto.cs
+++ DatingApp.api/Dtos/UserForDetailedDto.cs
@@ -1,16 +1,14 @@
 using System;
 using System.Collections.Generic;
 
-namespace DatingApp.api.Models
+namespace DatingApp.api.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
         public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -19,7 +17,7 @@ namespace DatingApp.api.Models
         public string Intrests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photos> Photos { get; set; }
-
+        public string PhotoUrl { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
