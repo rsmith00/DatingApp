1cdfc5810ccd8fc046e426df39909384632190d4
Rob Smith <rsmith00@outlook.com>
2020-07-16 11:05:10 -0500

    Built out members details interface.

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/user';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+    // tslint:disable-next-line: align
+    private route: ActivatedRoute) { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      // tslint:disable-next-line: no-string-literal
+      this.users = data['users'];
+    });
+  }
+
+
+  // tslint:disable-next-line: typedef
+  // loadUser(){
+  //   this.userService.getUsers().subscribe((users: User[]) => {
+  //     this.users = users;
+  //   }, error => {
+  //     this.alertify.error(error);
+  //   });
+  }
